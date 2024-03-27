c2eae001633b93b79a6bbd2b976f92e1f4ef9641
Israel Alfaro <israel.devalfaro@gmail.com>
2024-03-27 17:05:12 -0600

    feat: add not found validations and static acces to images

--- app.ts
+++ app.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import { creditRoutes } from './routes/creditRoutes';
+import AppError from './utils/AppError';
+import path from 'path';
 
 const APP_VERSION = process.env.APP_VERSION ?? 1;
 const API_PATH = `/api/v${APP_VERSION}`;
@@ -14,10 +16,16 @@ if (process.env.APP_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(`${API_PATH}/images`, express.static(path.join(__dirname, 'public', 'img', 'users')));
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
 app.use(API_PATH, creditRoutes);
+
+app.all('*', (req, _res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 export default app;
